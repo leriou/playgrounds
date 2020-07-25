c35c2a3e0ee74305582948f2a683255ff86afe25
leriou <yange1876@hotmail.com>
2020-07-25 23:19:06 +0800

    add test cases

--- py_playground/src/dp/knapsack_test.py
+++ py_playground/src/dp/knapsack_test.py
@@ -4,5 +4,20 @@ import unittest
 
 class TestKnapsack(unittest.TestCase):
 
+    def setUp(self) -> None:
+        '''
+        测试之前的准备工作
+        :return:
+        '''
+        pass
+
+    def tearDown(self) -> None:
+        '''
+        测试之后的收尾
+        如关闭数据库
+        :return:
+        '''
+        pass
+
     def test_result(self):
         self.assertEqual(Knapsack().knapsack(), 1)
