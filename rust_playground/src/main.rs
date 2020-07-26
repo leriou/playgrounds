7613e6207a725efa5d4bdbc187b7df9dcb74508b
leriou <yange1876@hotmail.com>
2020-07-26 10:53:14 +0800

    learning rust list

--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -1,5 +1,18 @@
 mod dp;
+mod linkedlist;
+
+use linkedlist::second::List;
 
 fn main() {
+    let op = Some(Box::new(8));
+
+    op.map(|x| println!("{:?}", x));
+
+    let mut a = "";
+    let mut b = "iisis";
+    let c = std::mem::replace(&mut a, b);
+    println!("a -> {:?}", a);
+    println!("b -> {:?}", b);
+    println!("c -> {:?}", c);
     println!("hello")
 }
