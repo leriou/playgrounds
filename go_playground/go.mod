9d655f02350f4ed579d295f7ad9f6b805e3fc266
leriou <yange1876@hotmail.com>
2020-08-14 20:35:50 +0800

    use go module for package manager

---
+++ go_playground/go.mod
@@ -0,0 +1,5 @@
+module go_playground
+
+go 1.14
+
+require github.com/gomodule/redigo v1.8.2
--- go_playground/main.go
+++ go_playground/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
 
 func main() {
-
-	fmt.Println("hello")
+	c, _ := redis.Dial("tcp", "127.0.0.1:6379")
+	defer c.Close()
+	n, _ := c.Do("GET", "10086")
+	fmt.Println(n)
 }
