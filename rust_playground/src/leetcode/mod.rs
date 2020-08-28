3c1ccd507bedc7862ba6e2422cf5504648be8a92
leriou <yange1876@hotmail.com>
2020-08-28 19:45:44 +0800

    solve no.84

--- rust_playground/src/leetcode/mod.rs
+++ rust_playground/src/leetcode/mod.rs
@@ -1,1 +1,2 @@
 pub mod no_679;
+pub mod no_84;
--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -2,4 +2,9 @@ mod dp;
 mod leetcode;
 mod unionfind;
 
-fn main() {}
+fn main() {
+    let mut v = vec![3, 2, 3];
+    v.insert(0, 0);
+    v.push(0);
+    println!("{:?}", v);
+}
