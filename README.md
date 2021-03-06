dde47716c169d2db8c2dbbc48fb3dbefe0e68cf7
leriou <leriou@lerioudeMacBook-Air.local>
2021-03-06 22:32:50 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # playgrounds
 private  code playgrounds
+
+
+
+m1芯片安装pandas
+arch -x86_64 /usr/local/bin/pip install pandas
--- rust_playground/src/main.rs
+++ rust_playground/src/main.rs
@@ -2,30 +2,21 @@ mod backtrace;
 mod dp;
 mod leetcode;
 mod unionfind;
-
-fn can_partition(nums: Vec<i32>) -> bool {
-    let mut sum = nums.iter().fold(0, |x1, x2| x1 + x2);
-    if sum % 2 == 1 {
-        return false;
-    }
-    sum >>= 1;
-    let (n, mut dp) = (nums.len(), vec![false; sum as usize + 1]);
-    dp[0] = true;
-    for i in 0..n {
-        for j in (0..=sum).rev() {
-            if j >= nums[i] {
-                let ju = j as usize;
-                dp[ju] = dp[ju] || dp[ju - nums[i] as usize];
-            };
-        }
-    }
-    dp[sum as usize]
-}
+use rand::prelude::*;
+use time;
 
 fn main() {
     let a = 0.1;
     let b = 0.2;
     println!("{:?}", a + b);
 
-    println!("{:?}", can_partition(vec![2, 3, 5]))
+    println!("{}", get_rand());
+
+    println!("{:?}", time::precise_time_s())
+}
+
+
+fn get_rand() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen::<f64>()
 }
